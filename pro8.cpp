b64c2ca8aa7d8f58e01007db0472dfc06992bd68
afzal.sorathiya <afzal.sorathiya@letsnurture.co.in>
2022-08-14 16:55:50 +0530

    add pro8 files

---
+++ pro8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int i;
+    for (i = 0; i < 1000000000; i++)
+    {
+        /* code */
+    std::cout << "error";
+    }
+    
+
+    return 0;
+}
